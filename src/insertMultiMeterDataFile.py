c1a46a636509a8f512eda892c3ac6e715c8bd6fd
Daniel Zhang (張道博) <dz1111@ikiapps.com>
2014-08-26 12:43:30 -1000

    moving meter names and data paths into a utility class

--- src/insertMultiMeterDataFile.py
+++ src/insertMultiMeterDataFile.py
@@ -22,13 +22,12 @@ from sek.logger import SEKLogger
 import argparse
 from insertSingleMeterDataFile import SingleFileLoader
 import multiprocessing
-import os
-import fnmatch
 import sys
+from si_util import SIUtil
 
 
 COMMAND_LINE_ARGS = None
-MULTIPROCESSING_LIMIT = 8
+MULTIPROCESSING_LIMIT = 6
 
 
 def processCommandLineArguments():
@@ -43,46 +42,50 @@ def processCommandLineArguments():
     COMMAND_LINE_ARGS = parser.parse_args()
 
 
-def pathsToProcess():
-    """
-    :return: List
-    """
-    global COMMAND_LINE_ARGS
-    pathsToProcess = []
-    for root, dirnames, filenames in os.walk(COMMAND_LINE_ARGS.basepath):
-        for filename in fnmatch.filter(filenames, '*.csv'):
-            # logger.log(filename, 'debug')
-            pathsToProcess.append(os.path.join(root, filename))
-    return pathsToProcess
-
-
 if __name__ == '__main__':
     logger = SEKLogger(__name__, 'debug')
+    siUtil = SIUtil()
     processCommandLineArguments()
 
-    paths = pathsToProcess()
+
+    paths = siUtil.pathsToProcess(COMMAND_LINE_ARGS.basepath)
+    lenPaths = len(paths)
+    finCnt = 0 # finished loading file count
+    rowCnt = 0
     assert len(paths) >= 1
 
-    def makeMeters():
-        meters = {}
-        for p in paths:
-            meterName = os.path.basename(os.path.dirname(p))
-            if meterName not in meters:
-                meters[meterName] = 1
 
-        for name in meters.keys():
+    def makeMeters():
+        for name in siUtil.meters():
             logger.log('Loading multi files for meter name {}.'.format(
                 SingleFileLoader().getMeterID(name)))
 
 
     def insertData(x):
+        global finCnt
+        global lenPaths
         logger.log('process {}'.format(str(multiprocessing.current_process())))
+        # logger.log('loading {} out of {}'.format(paths.index(x), lenPaths))
         SingleFileLoader(x).insertDataFromFile()
-        logger.log('finished loading {}'.format(x), 'debug')
+        finCnt += 1
+        logger.log('finished loading {}, total finished {}/{}'.format(x, finCnt,
+                                                                      lenPaths),
+                   'debug')
 
     makeMeters()
 
-    pool = multiprocessing.Pool(MULTIPROCESSING_LIMIT)
-    results = pool.map(insertData, paths)
-    pool.close()
-    pool.join()
+    # pool = multiprocessing.Pool(MULTIPROCESSING_LIMIT)
+    # results = pool.map(insertData, paths)
+    # pool.close()
+    # pool.join()
+
+    # Single core:
+    for p in paths:
+        rowCnt += SingleFileLoader(p).insertDataFromFile()
+        finCnt += 1
+        logger.log('finished loading {}, total finished {}/{}'.format(p, finCnt,
+                                                                      lenPaths),
+                   'debug')
+
+    logger.log('row cnt {}'.format(rowCnt))
+
---
+++ src/si_util.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Daniel Zhang (張道博)'
+__copyright__ = 'Copyright (c) 2014, University of Hawaii Smart Energy Project'
+__license__ = 'https://raw.github.com/Hawaii-Smart-Energy-Project/Smart-Grid' \
+              '-PV-Inverter/master/BSD-LICENSE.txt'
+
+from sek.logger import SEKLogger
+import os
+import fnmatch
+
+
+class SIUtil(object):
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self.logger = SEKLogger(__name__, 'INFO')
+
+
+    def pathsToProcess(self, basepath = ''):
+        """
+        :param basepath: String
+        :return: List
+        """
+        pathsToProcess = []
+        for root, dirnames, filenames in os.walk(basepath):
+            for filename in fnmatch.filter(filenames, '*.csv'):
+                pathsToProcess.append(os.path.join(root, filename))
+        return pathsToProcess
+
+
+    def meters(self, basepath = ''):
+        """
+        :param basepath: String
+        :return: List
+        """
+        meters = {}
+        for p in self.pathsToProcess():
+            meterName = os.path.basename(os.path.dirname(p))
+            if meterName not in meters:
+                meters[meterName] = 1
+        return meters.keys()
